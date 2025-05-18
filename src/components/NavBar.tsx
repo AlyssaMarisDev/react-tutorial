939731e6f365e9f1f32dd83217a3fdfc5c3dd8c8
alyssamarisdev@gmail.com <alyssamarisdev@gmail.com>
2025-05-18 11:59:48 -0700

    Feat: Add colour mode switch to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/GameHub Resources/Logo/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
       <Text>GameHub</Text>
+      <ColorModeButton />
     </HStack>
   );
 };
