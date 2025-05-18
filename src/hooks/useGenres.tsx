5d46015bcd89a9d0fe8230de127f44603ab46c3f
alyssamarisdev@gmail.com <alyssamarisdev@gmail.com>
2025-05-18 13:23:12 -0700

    Refactor: Clean up duplicated hook code

--- src/hooks/useGenres.tsx
+++ src/hooks/useGenres.tsx
@@ -1,11 +1,4 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-clients";
-import { CanceledError } from "axios";
-
-interface FetchGenresResponse {
-  count: number;
-  results: Genre[];
-}
+import useData from "./useData";
 
 export interface Genre {
   id: number;
@@ -13,30 +6,6 @@ export interface Genre {
   slug: string;
 }
 
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setIsLoading(true);
-    apiClient
-      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setIsLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
