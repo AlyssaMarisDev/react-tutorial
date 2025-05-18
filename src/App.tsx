ea00fc2d9bb078801a11215ff3f5c0a8c36f6fad
alyssamarisdev@gmail.com <alyssamarisdev@gmail.com>
2025-05-18 11:49:10 -0700

    Feat: Added nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show, useMediaQuery } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const [isMobile] = useMediaQuery(["(max-width: 768px)"]);
+
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show when={!isMobile}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
